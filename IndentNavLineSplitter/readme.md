c12998f7fb84362727e15afe2bc42953613bb622
mltony <anton.malykh+0@gmail.com>
2025-11-20 11:50:55 -0800

    Switching to x64 binary and NVDA 2026 compatibility

--- IndentNavLineSplitter/readme.md
+++ IndentNavLineSplitter/readme.md
@@ -1,3 +1,7 @@
+## Where to download python of a specific version:
+
+https://www.python.org/downloads/release/python-3139/
+
 ## When upgrading python version or adjusting build settings
 
 In Visual Studio 2022 go to Solution explorer, then right click on the project and click settings. Then:
@@ -8,4 +12,5 @@ In Visual Studio 2022 go to Solution explorer, then right click on the project a
     * `C:\Python311-32\libs`
 * Linker > Input > Additional dependencies
     * `python311.lib`
-    
+* C/C++ → Language → C++ Language Standard
+    * Set to C++ 17
--- buildVars.py
+++ buildVars.py
@@ -27,7 +27,7 @@ addon_info = {
 It allows NVDA users to navigate by indentation level or offset of lines or paragraphs.
 Also while editing source code in many programming languages, it allows to jump between the lines of the same indentation level, as well as quickly find lines with greater or lesser indentation level."""),
 	# version
-	"addon_version": "2.2.7",
+	"addon_version": "2.2.8",
 	# Author(s)
 	"addon_author": "Tony Malykh <anton.malykh@gmail.com>",
 	# URL for the add-on documentation support
@@ -37,9 +37,9 @@ Also while editing source code in many programming languages, it allows to jump 
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2019.2.0",
+	"addon_minimumNVDAVersion": "2026.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2025.1",
+	"addon_lastTestedNVDAVersion": "2026.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
